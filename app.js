de6286483ac196c82a1a9e9d694a5431c3aa26c1
Donabel <157907649+belram990@users.noreply.github.com>
2024-12-20 11:11:17 -0500

    added moment.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-///alert("hello");
+function updateTime() {
+  //Los Angeles
+
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss[<small>]A[</small]"
+  );
+
+  //Manila
+  let manilaElement = document.querySelector("#manila");
+  let manilaDateElement = manilaElement.querySelector(".date");
+  let manilaTimeElement = manilaElement.querySelector(".time");
+  let manilaTime = moment().tz("Asia/Manila");
+
+  manilaDateElement.innerHTML = manilaTime.format("MMMM Do YYYY");
+  manilaTimeElement.innerHTML = manilaTime.format("h:mm:ss[<small>]A[</small]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
